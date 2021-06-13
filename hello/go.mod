382ed9f54c34d7cf111db927a6a07235675b8247
Valliammai Subramanian <valliammai.kothai@gmail.com>
2021-06-13 17:59:51 -0400

    Update

--- hello/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/valliammai-subramanian/coding-questions-in-golang/hello
-
-go 1.16
-
-require github.com/valliammai-subramanian/coding-questions-in-golang/concatenate v0.0.0-20210613214757-9e3c81c2fb2d
--- hello/hello.go
+++ hello/hello.go
@@ -7,8 +7,7 @@ import (
 )
 
 func main() {
-    // Get a greeting message and print it.
-	ary1 := []int{1, 4, 5, 7}
+    ary1 := []int{1, 4, 5, 7}
 	ary2 := []int{2, 4, 6, 8}
 	output := []int{}
 	concatenate.Concatenation(ary1, ary2, &output)
