35d3bdde541012475527c7201ff66eb037d0a782
Valliammai Subramanian <valliammai.kothai@gmail.com>
2021-07-28 01:59:17 -0400

    Updates to kMeans test file

--- kMeans/kMeans_test.go
+++ kMeans/kMeans_test.go
@@ -3,7 +3,19 @@ package kMeans
 import (
 	"reflect"
 	"testing"
+	"sort"
 )
+
+type byPoints []Point
+func (p byPoints) Len() int {
+    return len(p)
+}
+func (p byPoints) Swap(i, j int) {
+    p[i], p[j] = p[j], p[i]
+}
+func (p byPoints) Less(i, j int) bool {
+    return p[i][0] < p[j][0] || ((p[i][0] == p[j][0]) && p[i][1] < p[j][1])
+}
 
 func Test_kmeans(t *testing.T) {
 	type args struct {
@@ -86,106 +98,107 @@ func Test_kmeans(t *testing.T) {
 	}
 }
 
-// func Test_classify(t *testing.T) {
-// 	type args struct {
-// 		points    []Point
-// 		centroids []Point
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want map[int][]Point
-// 	}{
-// 		{
-// 			name: "First Test",
-// 			args: args{
-// 				points: []Point{
-// 					Point{3, 3},
-// 					Point{2, 3},
-// 					Point{2, 2},
-// 					Point{1, 1},
-// 					Point{0, 0},
-// 					Point{1, -1},
-// 					Point{-1, -1},
-// 					Point{-3, -2},
-// 					Point{-2, 0},
-// 					Point{-2, -2},
-// 				},
-// 				centroids: []Point{
-// 					Point{2, 2},
-// 					Point{1, 1},
-// 					Point{-3, -2},
-// 					Point{-2, -2},
-// 				},
-// 			},
-// 			want: map[int][]Point{
-// 				0: []Point{
-// 					Point{3, 3},
-// 					Point{2, 3},
-// 					Point{2, 2},
-// 				},
-// 				1: []Point{
-// 					Point{1, 1},
-// 					Point{0, 0},
-// 					Point{1, -1},
-// 				},
-// 				2: []Point{
-// 					Point{-3, -2},
-// 					Point{-2, -2},
-// 				},
-// 				3: []Point{
-// 					Point{-1, -1},
-// 					Point{-2, 0},
-// 				},
-// 			},
-// 		},
-// 		{
-// 			name: "Second Test",
-// 			args: args{
-// 				points: []Point{
-// 					Point{4, 4},
-// 					Point{3, 3},
-// 					Point{2, 2},
-// 					Point{1, 1},
-// 					Point{0, 0},
-// 					Point{-1, -1},
-// 					Point{-2, -2},
-// 					Point{-3, -3},
-// 					Point{-4, -4},
-// 				},
-// 				centroids: []Point{
-// 					Point{3, 2},
-// 					Point{0, 0},
-// 					Point{-3, -2},
-// 				},
-// 			},
-// 			want: map[int][]Point{
-// 				0: []Point{
-// 					Point{4, 4},
-// 					Point{3, 3},
-// 					Point{2, 2},
-// 				},
-// 				1: []Point{
-// 					Point{1, 1},
-// 					Point{0, 0},
-// 					Point{-1, -1},
-// 				},
-// 				2: []Point{
-// 					Point{-2, -2},
-// 					Point{-3, -3},
-// 					Point{-4, -4},
-// 				},
-// 			},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got := classify(tt.args.points, tt.args.centroids)
-// 			for index, got_value := range got {
-// 				if tt.want[index] != got_value {
-// 					t.Errorf("classify() = %v, want %v", got, tt.want)
-// 				}
-// 			}
-// 		})
-// 	}
-// }
+func Test_classify(t *testing.T) {
+	type args struct {
+		points    []Point
+		centroids []Point
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[int][]Point
+	}{
+		{
+			name: "First Test",
+			args: args{
+				points: []Point{
+					Point{3, 3},
+					Point{2, 3},
+					Point{2, 2},
+					Point{1, 1},
+					Point{0, 0},
+					Point{1, -1},
+					Point{-1, -1},
+					Point{-3, -2},
+					Point{-2, 0},
+					Point{-2, -2},
+				},
+				centroids: []Point{
+					Point{2, 2},
+					Point{1, 1},
+					Point{-3, -2},
+					Point{-2, -2},
+				},
+			},
+			want: map[int][]Point{
+				0: []Point{
+					Point{2, 2},
+					Point{2, 3},
+					Point{3, 3},
+				},
+				1: []Point{
+					Point{0, 0},
+					Point{1, -1},
+					Point{1, 1},
+				},
+				2: []Point{
+					Point{-3, -2},
+				},
+				3: []Point{
+					Point{-2, -2},
+					Point{-2, 0},
+					Point{-1, -1},
+				},
+			},
+		},
+		{
+			name: "Second Test",
+			args: args{
+				points: []Point{
+					Point{4, 4},
+					Point{3, 3},
+					Point{2, 2},
+					Point{1, 1},
+					Point{0, 0},
+					Point{-1, -1},
+					Point{-2, -2},
+					Point{-3, -3},
+					Point{-4, -4},
+				},
+				centroids: []Point{
+					Point{3, 2},
+					Point{0, 0},
+					Point{-3, -2},
+				},
+			},
+			want: map[int][]Point{
+				0: []Point{
+					Point{2, 2},
+					Point{3, 3},
+					Point{4, 4},
+				},
+				1: []Point{
+					Point{-1, -1},
+					Point{0, 0},
+					Point{1, 1},
+				},
+				2: []Point{
+					Point{-4, -4},
+					Point{-3, -3},
+					Point{-2, -2},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := classify(tt.args.points, tt.args.centroids)
+			for index, points := range got {
+				sort.Sort(byPoints(points))
+				if !reflect.DeepEqual(tt.want[index], points) {
+					t.Errorf("classify() = %v, want %v", got, tt.want)				
+				}
+			}
+		})
+	}
+}
